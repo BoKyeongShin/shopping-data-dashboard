2e89c0860d0bb94bdbf9091207028ce70bd7bab7
BoKyeongShin <helenshin97@gmail.com>
2024-12-08 22:16:58 +0900

    feat: 메인 화면에 suspense 추가

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import { Box } from '@chakra-ui/react'
+import { AbsoluteCenter, Box, Spinner } from '@chakra-ui/react'
+import { Suspense } from 'react'
 import { CustomerTableSection } from './components/CustomerTable/CustomerTableSection'
 import { PriceFrequencyChartSection } from './components/PriceFrequencyChart/PriceFrequencyChartSection'
 
 function App() {
   return (
-    <Box>
-      {/* 전체 Suspense 감싸기 */}
-      <PriceFrequencyChartSection />
-      <CustomerTableSection />
+    <Box w="full" h="100vh">
+      <Suspense
+        fallback={
+          <AbsoluteCenter>
+            <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="gray.500" size="xl" />
+          </AbsoluteCenter>
+        }
+      >
+        <PriceFrequencyChartSection />
+        <CustomerTableSection />
+      </Suspense>
     </Box>
   )
 }
