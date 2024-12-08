91fdbfa1ede5b15e781265354f27f1299bd3493a
BoKyeongShin <helenshin97@gmail.com>
2024-12-08 21:22:06 +0900

    feat: CustomerDataModal props 추가

--- apps/frontend/src/components/CustomerDataModal/CustomerDataModal.tsx
+++ apps/frontend/src/components/CustomerDataModal/CustomerDataModal.tsx
@@ -1,17 +1,23 @@
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from '@chakra-ui/react'
+import { Customer } from '../../typedef/customer'
+import { Purchase } from '../../typedef/purchase'
 
 interface CustomerDataModalProps {
   isOpen: boolean
+  customer: Customer | null
+  purchases: Purchase[]
   onClose: () => void
 }
-export const CustomerDataModal: React.FC<CustomerDataModalProps> = ({ isOpen, onClose }) => {
+export const CustomerDataModal: React.FC<CustomerDataModalProps> = ({ isOpen, customer, purchases, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>title</ModalHeader>
+        <ModalHeader>구매 내역</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>Modal</ModalBody>
+        <ModalBody>
+          {customer?.name} - {purchases?.[0]?.product}
+        </ModalBody>
       </ModalContent>
     </Modal>
   )
